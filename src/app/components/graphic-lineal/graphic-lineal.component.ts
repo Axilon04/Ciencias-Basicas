a00a21294d902dc01483d84818fb891f681ef138
DuvanRGO <rozoparraduvan@gmail.com>
2022-09-11 22:28:24 -0500

    implementacion de grafica tangente dinamica

--- src/app/components/graphic-lineal/graphic-lineal.component.ts
+++ src/app/components/graphic-lineal/graphic-lineal.component.ts
@@ -1,55 +1,71 @@
-import { Component, OnInit, Input, OnChanges, ViewChild, ElementRef } from '@angular/core';
-
-import { ChartConfiguration, ChartOptions, ChartType, Chart } from "chart.js";
-
-import { grados,tangente} from './calcular';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from "chart.js";
+import { grados,tangente } from './calcular';
 
 @Component({
   selector: 'app-graphic-lineal',
   templateUrl: './graphic-lineal.component.html',
   styleUrls: ['./graphic-lineal.component.css']
 })
-export class GraphicLinealComponent implements OnInit, OnChanges {
+export class GraphicLinealComponent implements OnInit {
 
-  @ViewChild('tablet') table: ElementRef | undefined;
   @Input() numero:number = 360;
 
-
-
-
-
-  public destroyGraphicChart(){
-
+  public tableElement = () => {
+    // LLAMANDO ELEMENTO CANVAS
+    const element = document.querySelector('#tabla') as HTMLCanvasElement;
+    return element;
   }
 
-  public updateGraphic(){
-
-  }
-
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-    var myChart =  new Chart('tabla',{
+  public grafica = () => {
+    // CREACION DE LA GRAFICA USANDO LA LIBRERIA CHARTS JS
+    var pruebas = new Chart(this.tableElement() ,{
       type: 'line',
       data: {
         datasets: [{
-          data: tangente(this.numero),
-        }],
-        labels: grados(this.numero)
-      },
-      options: {
-        responsive: true,
-      }
+            data: tangente(this.numero),
+          }],
+          labels: grados(this.numero)
+        },
+        options: {
+          responsive: true,
+
+        }
     });
+    return pruebas;
   }
 
-  ngOnChanges(){
+  public updateGraphicChart(){
+    // ELIMINACION DEL CANVAS ANTERIOR PARA LA INTEGRACION DEL NUEVO CON DATOS ACTUALIZADOS
+    this.destroyGraphicChart();
 
+    // CREACION DE ELEMENTO SECTION PARA LA INTEGRACION/RECUPERACION DE LA GRAFICA (ELIMINE EL CANVAS Y CREE OTRO)
+    const sectionCanvas = document.querySelector('#contenedor-grafica-tangente') as HTMLCanvasElement;
+    const canvas =  document.createElement('canvas');
+    canvas.id = 'tabla';
+    sectionCanvas.appendChild(canvas)
+
+    // CREACION DE LA NUEVA GRAFICA CON LOS NUEVOS DATOS
+    this.grafica();
   }
 
+  public destroyGraphicChart(){
+    // REMOVIENDO EL CANVAS
+    this.tableElement().remove();
+  }
 
+  public graphic(): void{
+    // INICIALIZANDO GRAFICA PREDETERMINADA
+    this.grafica();
+    console.log("si mi pana estoy tratando de crear tabla");
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
 }
 
 
