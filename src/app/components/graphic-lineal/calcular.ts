50d45486aa7304e385e3678fcd7967ab6dc12544
MICHAEL-BETANCOURT <112732941+MICHAEL-BETANCOURT@users.noreply.github.com>
2022-09-15 12:47:22 -0500

    graficas
    implementacion de la grafica del seno en ciertos grados y la grafica del movimiento armonico simple

---
+++ src/app/components/graphic-lineal/calcular.ts
@@ -0,0 +1,64 @@
+//NPM INSTALL MATHJS
+import * as math from "mathjs";
+import { number } from "mathjs";
+
+// DOS FUNCIONES QUE AL INGRESAR UN GRADO REALICE LA RADIAL-(SENO) LOS GRADOS EN UN VECTOR Y LOS ESULTADOS EN OTRO VECTOR
+//ALMACENAMIENTO DE LOS GRADOS
+const grados = (numero:number) =>{
+  let grados =[];
+  if (numero>360) {
+    
+    console.log("Este valor no es permitido ingrese un valor menor a 360")
+
+  }
+  else{
+    for (let index =(numero); index >= 0; index-=5) {
+      grados.push(index);
+      }   
+  }
+    return grados;
+}
+//CALCULO DE LOS GRADOS INGRESADOS PARA LA FUNCION SENO
+const seno =(numero:number)=>{
+  let seno =[];
+  
+  for (let index =(numero); index >= -(numero); index-=5) {
+    seno.push(Math.sin(index*math.pi/180));
+    } 
+    return seno;  
+}
+
+
+//ENTREGA VIERNES
+//EJE X
+const amplitud =(tiempoF:number,tiempoR:number)=>{
+  let amplitud =[];
+  var count=0;
+  for (let index = (0); index <= (tiempoF); index+=tiempoR) {
+    count=index;
+    amplitud.push((count).toFixed(2)) ;
+
+    console.log(count);
+  }
+  
+  return amplitud;
+}
+//EJE Y
+//TIEMPOF EN CUANTO TIEMPO QUIRE LA GRAFICA
+//TIEMPOR EL PASO CON QUE VA A RECORRER LA GRAFICA
+const seno2 =(tiempoF:number,tiempoR:number,numero:number,omega:number,fi:number)=>{
+  let seno2 =[];
+  var ecuacion1:number;
+  var ecuacion2:number;
+  var ecuacion3:number;
+  for (let index =(0); index <= (tiempoF); index+=tiempoR) {
+    console.log(index)
+    ecuacion1=omega*index;
+    ecuacion2=ecuacion1+fi ;
+    ecuacion3=Math.sin(ecuacion2);
+    seno2.push(numero*ecuacion3);
+    } 
+    return seno2;  
+}
+export {amplitud,seno2}
+
---
+++ src/app/components/graphic-lineal/graphic-lineal.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from "chart.js";
+import { amplitud,seno2 } from './calcular';
+
+@Component({
+  selector: 'app-graphic-lineal',
+  templateUrl: './graphic-lineal.component.html',
+  styleUrls: ['./graphic-lineal.component.css']
+})
+export class GraphicLinealComponent implements OnInit {
+
+  @Input() numero:number | any;
+  @Input() tiempoF:number | any;
+  @Input() tiempoR:number | any;
+  @Input() omega:number | any;
+  @Input() fi:number | any;
+  public tableElement = () => {
+    // LLAMANDO ELEMENTO CANVAS
+    const element = document.querySelector('#tabla') as HTMLCanvasElement;
+    return element;
+  }
+
+  public grafica = () => {
+    // CREACION DE LA GRAFICA USANDO LA LIBRERIA CHARTS JS
+    var pruebas = new Chart(this.tableElement() ,{
+      type: 'line',
+      data: {
+        datasets: [{
+          //EJE Y
+            data: seno2(this.tiempoF,this.tiempoR,this.numero,this.omega,this.fi),
+          }],
+          // EJE X
+          labels: amplitud(this.tiempoF,this.tiempoR),
+        },
+        options: {
+          // responsive: true,
+          maintainAspectRatio: false,
+        }
+    });
+    console.log(seno2(this.tiempoF,this.tiempoR,this.omega,this.numero,this.fi))
+    console.log(amplitud(this.tiempoF,this.tiempoR))
+    return pruebas;
+
+  }
+
+  public updateGraphicChart(){
+    // ELIMINACION DEL CANVAS ANTERIOR PARA LA INTEGRACION DEL NUEVO CON DATOS ACTUALIZADOS
+    this.destroyGraphicChart();
+
+    // CREACION DE ELEMENTO SECTION PARA LA INTEGRACION/RECUPERACION DE LA GRAFICA (ELIMINE EL CANVAS Y CREE OTRO)
+    const sectionCanvas = document.querySelector('#contenedor-grafica-tangente') as HTMLCanvasElement;
+    const canvas =  document.createElement('canvas');
+    canvas.id = 'tabla';
+    sectionCanvas.appendChild(canvas)
+
+    // CREACION DE LA NUEVA GRAFICA CON LOS NUEVOS DATOS
+    this.grafica();
+    
+  }
+
+  public destroyGraphicChart(){
+    // REMOVIENDO EL CANVAS
+    this.tableElement().remove();
+  }
+
+  public graphic(): void{
+    // INICIALIZANDO GRAFICA PREDETERMINADA
+    this.grafica();
+    console.log("si mi pana estoy tratando de crear tabla");
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.grafica();
+    
+  }
+}
+
+
